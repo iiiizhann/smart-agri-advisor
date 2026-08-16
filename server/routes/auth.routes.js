52b38ca5b9e2b20828bfc54ccd4680f7466b4703
Izhan <izhanshaikh2225@gmail.com>
2026-08-16 13:53:25 +0530

    fix: update middleware path to plural

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, getMe } = require('../controllers/auth.controller');
-const { protect } = require('../middleware/auth.middleware');
-
+const { protect } = require('../middlewares/auth.middleware');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
